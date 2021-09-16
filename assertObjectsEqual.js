81fe56641ded4412c36b1895ad3f6531c75a5673
ramanmand <mandr@sheridancollege.ca>
2021-09-15 22:14:03 -0400

    assert object equal created

---
+++ assertObjectsEqual.js
@@ -0,0 +1,11 @@
+const eqObjects = require('./eqObjects');
+
+// applying function 
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected))
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  else
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+};
