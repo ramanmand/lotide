4e8b5c719c40575b83befea59886fc5ffe7264f0
ramanmand <mandr@sheridancollege.ca>
2021-09-20 22:22:57 -0400

    added mocha&chai testing

--- head.js
+++ head.js
@@ -1,16 +1,17 @@
+//const assertEqual = require('./assertEqual');
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
+if (actual === expected) {
     console.log(` ✅✅✅Assertion Passed: ${actual} === ${expected}`);
 
-  }
-  else {
+  } else {
     console.log(`🛑🛑🛑 Assertion Failed : ${actual} !== ${expected}`);
-  }
-};
+}
+ };
 
-const head = function(array){
-return array[0];
+const head = function(array) {
+  return array[0];
 };
+module.exports = head;
 //assertEqual(head([5,6,7]),5);
 //assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 
