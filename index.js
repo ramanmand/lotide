409b54856aefd42d2da05490c94ee6a3fd67e4b6
ramanmand <mandr@sheridancollege.ca>
2021-09-21 00:20:51 -0400

    index file

---
+++ index.js
@@ -0,0 +1,33 @@
+const assertArraysEqual   = require('./assertArraysEqual');
+const assertEqual         = require('./assertEqual');
+const assertObjectsEqual  = require('./assertObjectsEqual');
+const countLetters        = require('./countLetters');
+const countOnly           = require('./countOnly');
+const eqArrays            = require('./eqArrays');
+const eqObjects           = require('./eqObjects');
+const findKey             = require('./findKey');
+const findKeyByValue      = require('./findKeyByValue');
+const letterPositions     = require('./letterPositions');
+const map                 = require('./map');
+const takeUntil           = require('./takeUntil');
+const head                = require('./head');
+const tail                = require('./tail');
+const middle              = require('./middle');
+
+module.exports = {
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  map,
+  takeUntil,
+  head,
+  tail,
+  middle
+};
